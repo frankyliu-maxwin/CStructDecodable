3d91c8152c200edfcc619f2196474f5206c70f99
yume190 <yume190@gmail.com>
2017-12-27 18:28:00 +0800

    add lib

---
+++ CStructDecodable/CStructDecodable.h
@@ -0,0 +1,19 @@
+//
+//  CStructDecodable.h
+//  CStructDecodable
+//
+//  Created by Yume on 2017/12/27.
+//  Copyright © 2017年 Yume. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CStructDecodable.
+FOUNDATION_EXPORT double CStructDecodableVersionNumber;
+
+//! Project version string for CStructDecodable.
+FOUNDATION_EXPORT const unsigned char CStructDecodableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CStructDecodable/PublicHeader.h>
+
+
